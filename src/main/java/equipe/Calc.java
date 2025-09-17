3018c1ae1dc97fe51636ffcce38bb7039bc1a40e
Lucas Silva <lucassilvarodrigss16@gmail.com>
2025-09-17 23:22:51 +0000

    feat: implementar soma e atualizar equipe

--- src/main/java/equipe/Calc.java
+++ src/main/java/equipe/Calc.java
@@ -3,8 +3,8 @@ package com.exemplo.equipe;
 public class Calc {
     // Pessoa A
     public int soma(int a, int b) {
-        // TODO: implementar
-        return 0; 
+        
+        return a + b; 
     }
 
     // Pessoa B
